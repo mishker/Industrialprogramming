fb6caef1418297e66bb1f0d337525dff4f750568
Mikhail Zavgorodnii <93056322+mishker@users.noreply.github.com>
2023-03-06 18:22:47 +0300

    pr3

--- week2/ex2-3/rational.cpp
+++ week2/ex2-3/rational.cpp
@@ -16,7 +16,7 @@ int gcd(int a, int b) {
 RationalValues formating(int a, int b) {
 	if (b == 0)
 	{
-		std::cout << "знаменатель не должен быть равен нул";
+		std::cout << "знаменатель не должен быть равен нулю";
 		throw std::runtime_error("zero division error");
 	}
 
